42bc875e52b1cabd43fcfe65001d4f66c8b158c9
Amin Sadeghi <amin.sadeghi@live.com>
2025-06-10 10:31:20 -0700

    fix: update Result class and tortuosity_fd function to include effective diffusivity and verbose logging

--- src/poromics/_metrics.py
+++ src/poromics/_metrics.py
@@ -21,27 +21,38 @@ _taujl = julia_helpers.import_backend(_jl)
 class Result:
     """Container for storing the results sof a tortuosity simulation."""
 
-    def __init__(self, im, axis, tau, c):
+    def __init__(self, im, axis, tau, D_eff, c, D=None):
         """
         Initializes a tortuosity result object.
 
         Args:
-            tau (float): The tortuosity value.
-            c_grid (ndarray): The concentration grid solution.
+            im (ndarray): The boolean image used in the simulation.
             axis (int or str): The axis along which tortuosity was calculated.
+            tau (float): The tortuosity value.
+            D_eff (float): The effective diffusivity value.
+            c (ndarray): The concentration solution reshaped to the image shape.
+            D (ndarray): The diffusivity field used in the simulation.
 
         """
         self.im = im
         self.axis = axis
         self.tau = tau
+        self.D_eff = D_eff
         self.c = c
+        self.D = D
 
     def __repr__(self):
-        return f"Result(τ = {self.tau:.2f}, axis = {self.axis})"
+        return f"Result(τ = {self.tau:.2f}, axis = {self.axis}, variable D = {self.D is not None})"
 
 
 def tortuosity_fd(
-    im, *, axis: int, D: np.ndarray = None, rtol: float = 1e-5, gpu: bool = False
+    im,
+    *,
+    axis: int,
+    D: np.ndarray = None,
+    rtol: float = 1e-5,
+    gpu: bool = False,
+    verbose: bool = False,
 ) -> Result:
     """
     Performs a tortuosity simulation on the given image along the specified axis.
@@ -55,6 +66,7 @@ def tortuosity_fd(
         D (ndarray): Diffusivity field. If None, a uniform diffusivity of 1.0 is assumed.
         rtol (float): Relative tolerance for the solver.
         gpu (bool): If True, use GPU for computation.
+        verbose (bool): If True, print additional information during the solution process.
 
     Returns:
         result: An object containing the boolean image, axis, tortuosity, and
@@ -75,7 +87,8 @@ def tortuosity_fd(
             D[~im] = 0.0
         logger.warning("The image has been trimmed to ensure percolation.")
     sim = _taujl.TortuositySimulation(im, D=D, axis=axis_jl, gpu=gpu)
-    sol = _taujl.solve(sim.prob, _taujl.KrylovJL_CG(), verbose=False, reltol=rtol)
+    sol = _taujl.solve(sim.prob, _taujl.KrylovJL_CG(), verbose=verbose, reltol=rtol)
     c = _taujl.vec_to_grid(sol.u, im)
     tau = _taujl.tortuosity(c, axis=axis_jl, D=D)
-    return Result(np.asarray(im), axis, tau, np.asarray(c))
+    D_eff = _taujl.effective_diffusivity(c, axis=axis_jl, D=D)
+    return Result(np.asarray(im), axis, tau, D_eff, np.asarray(c), D)
